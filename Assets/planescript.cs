30abc8b393f6acc590e3fb50ce7571754cf9c929
David Karn <davidkarnseattle@gmail.com>
2017-02-25 12:32:03 -0800

    unity project initial import

---
+++ Assets/planescript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class planescript : MonoBehaviour {
+	IEnumerator Start () {
+		string url = "https://i.imgflip.com/1kc3j8.jpg";
+		WWW www = new WWW(url);
+		yield return www;
+
+		GetComponent<Renderer>().material.mainTexture = www.texture;
+		load_image ();
+	}
+
+	IEnumerator load_image() {
+		Debug.Log ("loading an image");
+		string url = "https://i.imgflip.com/1kc3j8.jpg";
+		WWW www = new WWW(url);
+		yield return www;
+
+		GetComponent<Renderer>().material.mainTexture = www.texture;
+	}
+
+	void Update () {
+		
+	}
+}
