1efafc3fd9a4e459f0f364a01200ca882c6ce9f0
David Karn <davidkarnseattle@gmail.com>
2017-02-25 17:07:59 -0800

    pull image from json

--- Assets/planescript.cs
+++ Assets/planescript.cs
@@ -6,19 +6,24 @@ using System.Linq;
 using System.Xml.Linq;
 using System;
 using System.Text;
+using System.Threading;
+[System.Serializable]
 
 
 public class ImgResponse
 {
 	public string image;
+	public string status;
+	public string ReceivedOn;
+	public string RecievedBy;
 
 	public static ImgResponse CreateFromJSON(string jsonString)
 	{
-		return JsonUtility.FromJson<ImgResponse>(jsonString);
+		ImgResponse x = JsonUtility.FromJson<ImgResponse>(jsonString);
+		return x;
 	}
 
 	public Texture2D get_texture() {
-
 		Texture2D texture = new Texture2D (1, 1);
 		texture.LoadImage (Convert.FromBase64String (image)); 
 		return texture;
@@ -30,15 +35,30 @@ public class planescript : MonoBehaviour {
 	string url = "http://i.imgur.com/6g4wY8F.png";
 
 	IEnumerator Start () {
-		WWW www = new WWW(url);
-		yield return www;
-		GetComponent<Renderer>().material = new Material(Shader.Find("Unlit/Transparent"));
+		/*WWW www = new WWW(url);
 
-		GetComponent<Renderer>().material.mainTexture = www.texture;
+		yield return www;
+		GetComponent<Renderer>().material = new Material(Shader.Find("Unlit/Transparent"));*/
+
+		string image_url = "http://dev1-api.prolifiq.com/muffin/api/calendar/101?month=march";
+		var headers = new Hashtable();
+		headers.Add("Content-Type", "application/json");
+		WWW response = new WWW(image_url);
+		yield return response;
+
+		//string dummy = "{image: \"asdf\", status:\"asdf\",ReceivedOn: \"skldfjsdlkf\", RecievedBy: \"sdkfj\"}";
+
+		ImgResponse r = ImgResponse.CreateFromJSON(response.text);
+		GetComponent<Renderer>().material = new Material(Shader.Find("Unlit/Transparent"));
+		GetComponent<Renderer>().material.mainTexture = r.get_texture(); 
+
+		//GetComponent<Renderer>().material.mainTexture = www.texture;
 	}
 
 	IEnumerator get_image() {
-		string image_url = "http://dev1-api.prolifiq.com/muffin/api/calendar/101";
+		string image_url = "http://dev1-api.prolifiq.com/muffin/api/calendar/101?month=march";
+		var headers = new Hashtable();
+		headers.Add("Content-Type", "application/json");
 		WWW response = new WWW(image_url);
 		yield return response;
 
