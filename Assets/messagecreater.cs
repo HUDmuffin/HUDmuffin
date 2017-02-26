d56bd59c6158d19065f78ae82439bfdd13143382
David Karn <davidkarnseattle@gmail.com>
2017-02-26 10:45:21 -0800

    add images for message overlays

--- Assets/messagecreater.cs
+++ Assets/messagecreater.cs
@@ -4,19 +4,32 @@ using UnityEngine;
 
 public class messagecreater : MonoBehaviour {
 	float timeLeft = 1.0f;
+	string[] urls =	 new string[] {"http://i.imgur.com/LuewFKJ.png",
+					 "http://i.imgur.com/bmqnPN0.png",
+		"http://i.imgur.com/L3d83qb.png"};
+	Texture[] textures = new Texture[] { null, null, null };
+	int cururl = 0;
 	ImgResponse r;
+	WWW resp;
 
 	// Use this for initialization
 	IEnumerator Start () {
 		string image_url = "http://dev1-api.prolifiq.com/muffin/api/calendar/101?month=january";
 		var headers = new Hashtable();
+
 		headers.Add("Content-Type", "application/json");
 		WWW response = new WWW(image_url);
 		yield return response;
 		print ("got response");
 		Debug.Log(response.text);
 		r = ImgResponse.CreateFromJSON(response.text);
-	}
+
+
+		for (var i = 0; i < urls.Length; i++) {
+			resp = new WWW(urls[i]);
+			yield return resp;
+			
+			textures[i] = resp.texture; }}
 
 	Mesh CreateMesh(float width, float height)
 	{
@@ -49,20 +62,24 @@ public class messagecreater : MonoBehaviour {
 			timeLeft = 2.0f;
 			Debug.Log ("making plane");
 			GameObject plane = GameObject.CreatePrimitive(PrimitiveType.Plane);
-			plane.transform.position = new Vector3(0, 0, 20);
-			plane.transform.Rotate (new Vector3 (0, 180, 0));
+			//plane.transform.position = new Vector3(0, 0, 20);
+			//plane.transform.Rotate (new Vector3 (0, 180, 0));
 
 			MeshFilter meshFilter = plane.GetComponent<MeshFilter> ();
-			meshFilter.mesh = CreateMesh(10, 3);
+			meshFilter.mesh = CreateMesh(1.5f, 10);
 			//MeshRenderer renderer = plane.AddComponent(typeof(MeshRenderer)) as MeshRenderer;
 			//renderer.material.shader = Shader.Find ("Mobile/Particles/Alpha Blended");
 			plane.transform.parent = this.transform;
 			plane.GetComponent<Renderer>().material = new Material(Shader.Find("Mobile/Particles/Alpha Blended"));
 
 			//plane.GetComponent<Renderer>().material.shader = Shader.Find ("Mobile/Particles/Alpha Blended");
-			plane.GetComponent<Renderer>().material.mainTexture = r.get_texture(); 
+			plane.GetComponent<Renderer>().material.mainTexture = textures[cururl];
+			cururl += 1;
+			if (cururl >= textures.Length) {
+				cururl = 0;
+			}
 			plane.transform.position = new Vector3(0, 2.5f, 20);
-			plane.transform.Rotate (new Vector3 (0, 180, 0));
+			plane.transform.Rotate (new Vector3 (0, 180, 90));
 
 			Debug.Log ("goint to loop");
 			//Renderer[] ChildrenRenderer = GetComponent().GetComponentsInChildren(typeof(Renderer));
@@ -71,7 +88,7 @@ public class messagecreater : MonoBehaviour {
 			for (int i = 0; i < transform.childCount - 1; i++) {
 				Debug.Log ("running");
 				Debug.Log (i);
-				transform.GetChild (i).transform.Translate (0, -3.25f, 0);
+				transform.GetChild (i).transform.Translate (-3.25f, 0, 0);
 				if (i == 0 && transform.childCount >= 6) {
 					Debug.Log ("removing");
 					Destroy (transform.GetChild (i).gameObject, 0.25f); }}}}}
