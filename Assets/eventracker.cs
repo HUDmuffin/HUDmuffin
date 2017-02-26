6524fecbc8e7ee0c3d10440c08749cc8b014494f
David Karn <davidkarnseattle@gmail.com>
2017-02-25 22:00:20 -0800

    show and hide HUD display in quad when a target is matched/moves off screen

---
+++ Assets/eventracker.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class eventracker : MonoBehaviour, ITrackableEventHandler
+{
+	private TrackableBehaviour mTrackableBehaviour;
+	public Plane hud;
+
+	void Start()
+	{
+		mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+		if (mTrackableBehaviour)
+		{
+			mTrackableBehaviour.RegisterTrackableEventHandler(this);
+		}
+	}
+
+	public void OnTrackableStateChanged(
+		TrackableBehaviour.Status previousStatus,
+		TrackableBehaviour.Status newStatus)
+	{
+		if (newStatus == TrackableBehaviour.Status.DETECTED ||
+			newStatus == TrackableBehaviour.Status.TRACKED ||
+			newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+		{
+			var player = GameObject.FindWithTag("quad");
+			player.transform.position = new Vector3 (900, 900, 900);
+		}
+		else
+		{
+			var player = GameObject.FindWithTag("quad");
+			player.transform.position = new Vector3 (0, 0, 20);
+		}
+	}   
+}
--- Assets/quadscript.cs
+++ Assets/quadscript.cs
@@ -1,16 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Xml;
+using System.Linq;
+using System.Xml.Linq;
+using System;
+using System.Text;
+using System.Threading;
+[System.Serializable]
 
 public class quadscript : MonoBehaviour {
+	string url = "http://i.imgur.com/6g4wY8F.png";
 
-	// Use this for initialization
-	void Start () {
-		
+	IEnumerator Start () {
+		string image_url = "http://dev1-api.prolifiq.com/muffin/api/hud?message=welcome";
+		var headers = new Hashtable();
+		headers.Add("Content-Type", "application/json");
+		WWW response = new WWW(image_url);
+		yield return response;
+
+		ImgResponse r = ImgResponse.CreateFromJSON(response.text);
+		GetComponent<Renderer>().material = new Material(Shader.Find("Mobile/Particles/Alpha Blended"));
+		GetComponent<Renderer>().material.mainTexture = r.get_texture(); 
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
-		
+
 	}
 }
